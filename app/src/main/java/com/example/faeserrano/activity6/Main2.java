2918db8cd19da99e4d741f1c5bda51628d6ded02
Fae <FaeSerrano@Faes-MacBook-Air.local>
2016-11-17 22:07:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/faeserrano/activity6/Main2.java
@@ -0,0 +1,20 @@
+package com.example.faeserrano.activity6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView txtv = (TextView)findViewById(R.id.textView);
+
+        Bundle b = getIntent().getExtras();
+        String user = b.getString("username");
+        String pass = b.getString("password");
+        txtv.setText("user:" + user + "\npass: " + pass);
+    }
+}
